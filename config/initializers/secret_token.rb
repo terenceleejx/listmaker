48491096ea5641279466fcb49a1abbf9a97527d0
Terence Lee <terence.lee.jx@gmail.com>
2013-11-13 01:23:34 +0800

    installed foundation, devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Listmaker::Application.config.secret_key_base = '51a0643561fbda971f50491e9db455bd7e57f622beccadf40f5580c098dd87aefbc57aac030d2e604b1f72be3a493c793f64c44579196cd990a62e757139e8eb'
