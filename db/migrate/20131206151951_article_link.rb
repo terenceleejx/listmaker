dbb18f109e1a5f27ad8090e5107b240cb1d09e77
Terence Lee <terence.lee.jx@gmail.com>
2013-12-07 10:49:28 +0800

    added migration for url

--- db/migrate/20131206151951_article_link.rb
+++
@@ -1,7 +0,0 @@
-class ArticleLink < ActiveRecord::Migration
-  def change
-  	change_column :china_articles, :summary1, :text
-  	change_column :china_articles, :summary2, :text
-  	change_column :china_articles, :summary3, :text
-  end
-end
---
+++ db/migrate/20131206151952_article_link.rb
@@ -0,0 +1,5 @@
+class ArticleLink < ActiveRecord::Migration
+  def change
+  	add_column :china_articles, :url, :string
+  end
+end
