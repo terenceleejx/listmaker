4de14756142751bbc0f26ecbe5830ae695288c1b
Terence Lee <terence.lee.jx@gmail.com>
2014-06-28 00:52:41 +0800

    sg time zone

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Listmaker
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.local_zone
+    config.time_zone = 'Singapore'
     config.filter_parameters += [:password]
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
